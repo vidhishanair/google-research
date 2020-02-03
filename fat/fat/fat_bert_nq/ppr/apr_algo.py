bcd57b0aa86fdcd08787599bb6e2fbd6b598b4fe
Vidhisha Balachandran <vidhishanair@gmail.com>
2020-02-02 22:28:56 -0500

    fixed num hop typo

--- fat/fat/fat_bert_nq/ppr/apr_algo.py
+++ fat/fat/fat_bert_nq/ppr/apr_algo.py
@@ -232,7 +232,7 @@ def csr_get_all_paths(question_seeds, adj_mat, answer_seeds, rel_dict, k_hop):
   path = []
   for hop in range(num_hops):
     new_paths = []
-    for object in answer_seeds_found_dict[hop]:
+    for object in answer_seeds_found_dict[hop+1]:
       path.append([(None, None, object)])
     for i in range(len(path)):
       object = path[i][-1][2]
