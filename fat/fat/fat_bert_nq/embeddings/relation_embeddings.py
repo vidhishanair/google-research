d6b0c6c7be748c78a9bdd874e2c2a4c092bff167
Vidhisha Balachandran <vidhishanair@gmail.com>
2020-02-16 17:21:15 -0500

    Added relation embedding creation

---
+++ fat/fat/fat_bert_nq/embeddings/relation_embeddings.py
@@ -0,0 +1,52 @@
+"""Script to compute relation embeddings for given relation."""
+#import cPickle as pkl
+import pickle as pkl
+import numpy as np
+import json
+from tqdm import tqdm
+import tensorflow as tf
+import gzip
+from fat.fat_bert_nq import nq_data_utils
+
+flags = tf.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('relations_file', None, 'input relations dict')
+flags.DEFINE_string('output_fule', None, 'output relations dict')
+
+relations_file = FLAGS.relations_file
+embeddings_file = "/remote/bones/user/vbalacha/datasets/glove/glove.6B.300d.txt"
+output_file = FLAGS.output_file
+dim = 300
+
+word_to_relation = {}
+relation_lens = {}
+
+def _add_word(word, v):
+    if word not in word_to_relation: word_to_relation[word] = []
+    word_to_relation[word].append(v)
+    if v not in relation_lens: relation_lens[v] = 0
+    relation_lens[v] += 1
+
+with gzip.GzipFile(fileobj=tf.gfile.Open(relations_file, 'rb')) as op4:
+    obj = json.load(op4)
+    op4.close()
+    relations = obj['r']
+    for rel_id, val in relations_file.items():
+        rel_name = val['name']
+        for word in rel_name.split():
+            _add_word(word, rel_id)
+
+relation_emb = {r: np.zeros((dim,)) for r in relation_lens}
+with open(embeddings_file) as f:
+    for line in tqdm(f):
+        word, vec = line.strip().split(None, 1)
+        if word in word_to_relation:
+            for qid in word_to_relation[word]:
+                relation_emb[qid] += np.array([float(vv) for vv in vec.split()])
+
+for relation in relation_emb:
+    relation_emb[relation] = relation_emb[relation] / relation_lens[relation]
+
+print("Processed relations: " + str(len(relation_emb.keys())))
+pkl.dump(relation_emb, open(output_file, "wb"))
